94d338da2b5c3d838cb167dc485f3ad5ff4b66c3
30vam <khodaparast2011@gmail.com>
2024-07-17 15:13:11 +0330

    Refactored abit

--- src/customSelect.js
+++ src/customSelect.js
@@ -1,18 +1,28 @@
 const dropdowns = document.getElementsByClassName('dropdown');
+const body = document.getElementsByTagName('body')[0];
+
+// Change inputElement text when clicked inside the optionBox
+const selectOption = (e, inputElement, dropdown) => {
+    e.stopPropagation();
+    const selectedElement = e.target;  // Find the element that was clicked on
+    
+    // First check if the user didn't accidentally click on an empty space in the optionBox element that wans't an option
+    if (!selectedElement.classList.contains('optionbox')) {
+        inputElement.value = selectedElement.innerText;  // Sets the value for <inout> html to selected option
+        dropdown.classList.remove('opened');  // Close the optionBox
+    }
+};
 
 for (const dropdown of dropdowns) {
     const inputElement = dropdown.getElementsByTagName('input')[0];
     const optionBox = dropdown.getElementsByTagName('ul')[0];
 
     // When input is clicked on, .opened class will be toggled.
-    inputElement.addEventListener('click', function () {
+    inputElement.addEventListener('click', function (e) {
         dropdown.classList.toggle('opened');
     });
 
-    // Change inputElement text when clicked inside the optionBox
-    optionBox.addEventListener('click', function (e) {
-        const selectedText = e.target.innerText;  // Find the text that was selected using event delegation
-        inputElement.value = selectedText;  // Sets the value for <inout> html to selected option
-        dropdown.classList.remove('opened');  // Close the optionBox
-    });
+    // Add the event listeners
+    
+    optionBox.addEventListener('click', (e) => selectOption(e, inputElement, dropdown));
 }
