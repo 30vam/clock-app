af9402fb57c83fff65a0ca890a68a295ad8ce6ef
30vam <khodaparast2011@gmail.com>
2024-07-19 11:50:15 +0330

    Added all timezones to the dropdown

--- src/clock.js
+++ src/clock.js
@@ -1,20 +1,40 @@
+// Elements
 const timeDisplay = document.getElementById('time').childNodes[0];
 const dateDisplay = document.getElementById('date');
+const timezoneDropdown = document.getElementById('timezone-dropdown');
+
+// Variables
+const timeFormat = 'HH:mm:ss';
+const dateFormat = 'MMMM DD, YYYY';
 let now = null;
+const timezones = moment.tz.names();
+const timezoneMap = new Map(); // Key(left side) is moment timezone name, and value(right side) is an the function: now.tz(name)
 
-const updateTime = function() {
+function addTimezones() {
+    const optionbox = timezoneDropdown.querySelector('.optionbox');  // Get the .optionbox object
+    for (const timezone of timezones) {
+        // Add each timezone to a map in a correct format
+        const optionName = timezone;  // Create the name that actually appears in the dropdown
+        timezoneMap.set(optionName, () => { now.tz(timezone) });
+        
+        // Create a new timezone option div and add it to dropdown
+        const optionDiv = document.createElement('div');
+        optionDiv.classList.add('option');
+        optionDiv.textContent = optionName;
+        optionbox.append(optionDiv);
+    }
+}
+
+function updateTime() {
     now = moment();
-    timeDisplay.nodeValue = now.tz('America/Los_Angeles').format('HH:mm:ss');
-};
+    timeDisplay.nodeValue = now.tz('America/Los_Angeles').format(timeFormat);
+}
 
-const updateDate = function() {
-    dateDisplay.innerText = now.tz('America/Los_Angeles').format('MMMM DD, YYYY');
-};
+function updateDate() {
+    dateDisplay.innerText = now.tz('America/Los_Angeles').format(dateFormat);
+}
 
-const addTimezones = function() {}
-
+addTimezones();
 updateTime();
 updateDate();
-
-//Update time every 1000ms
-setInterval(updateTime, 1000);
+setInterval(updateTime, 1000);  // Update time every 1000ms
