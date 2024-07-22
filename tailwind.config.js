f348b3ef6d54b01231de8a4dca07099deef03725
30vam <khodaparast2011@gmail.com>
2024-07-22 22:28:00 +0330

    Attempt to fix font family on safari

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
-      fontFamily: { sans : ['Montserrat', 'sans-serif'] }
+      fontFamily: { 
+        mainFont : ['Montserrat', 'sans-serif'] 
+      }
     },
   },
   plugins: [],
