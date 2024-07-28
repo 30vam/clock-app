5d14a440a7521fea0f04041cb5e91e4674230c9d
30vam <khodaparast2011@gmail.com>
2024-07-29 00:30:35 +0330

    Added event for clicking on tabs

--- src/tabWidget.js
+++ src/tabWidget.js
@@ -1,5 +1,16 @@
 const tabWidgets = document.querySelectorAll('.tabwidget');
 
+function switchTab(e) {
+    const currentTab = e.target;
+    e.stopPropagation();
+    //Check if the tab was active first
+    console.log(currentTab);
+}
+
+function createTabWidgetMap() {
+    
+}
+
 // For all .tabwidget objects
 for (const tabWidget of tabWidgets) {
     const tabWidgetNav = tabWidget.querySelector('.tabwidget-nav');
@@ -17,8 +28,12 @@ for (const tabWidget of tabWidgets) {
 
         currentModule.style.display = 'none';  // At first, disable all modules inside the tabWidget
         tabWidgetMap.set(currentTab, currentModule);
+
+        currentTab.addEventListener('click', e => switchTab(e));  // Event Listener for when user clicks on a tab
     }
 
     // Only make the first module visible at first (First value is selected using spread...)
     [...tabWidgetMap][0][1].style.display = '';  // '' makes the display to return to whatever it was normally (For example flexbox in this case instead of block)
+
+    
 }
