a3390ef79aed23764422665f1f1ef0180355eefe
30vam <khodaparast2011@gmail.com>
2024-07-28 21:35:55 +0330

    Binded tabs and modules using a map

--- src/tabWidget.js
+++ src/tabWidget.js
@@ -2,17 +2,19 @@ const tabWidgets = document.querySelectorAll('.tabwidget');
 
 // For all .tabwidget objects
 for (const tabWidget of tabWidgets) {
-    const tabWidgetNav = tabWidget.children.querySelector('.tabwidget-nav');
+    const tabWidgetNav = tabWidget.querySelector('.tabwidget-nav');
     const tabWidgetTabs = tabWidgetNav.children;  // List of all tabs
-    const tabWidgetContent = tabWidget.children.querySelector('.tabwidget-content');
+    const tabWidgetContent = tabWidget.querySelector('.tabwidget-content');
     const tabWidgetModules = tabWidgetContent.children;  // List of all modules
 
+    // Global variables
     const tabWidgetMap = new Map();
 
     // Create a map that binds tabs and modules
-    for (i == 0; i < tabWidgetTabs.length; i++) {
-        console.log(tabWidgetTabs.length)
+    for (let i = 0; i < tabWidgetTabs.length; i++) {
+        tabWidgetMap.set(tabWidgetTabs[i], tabWidgetModules[i]);
     }
+    console.log(tabWidgetMap);
 
     // Disable all modules inside the tabWidget except the first one
 }
